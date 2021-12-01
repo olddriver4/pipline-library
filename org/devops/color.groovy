a0e5ed7ed4a1fda9d6ba8ace8efaeab051bcc831
Li.Meng <1784238945@qq.com>
2021-12-01 16:51:51 +0800

    push

--- org/devops/color.groovy
+++ org/devops/color.groovy
@@ -1,6 +1,6 @@
 package org.devops
 
-def PrintColor(value,color){
+def color(value,color){
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m ${value} \033[0m",
               'green' : "^[[1;32m>>>>>>>>>>${value}>>>>>>>>>>^[[m",
