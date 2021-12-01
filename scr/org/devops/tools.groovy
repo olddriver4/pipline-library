979c63974919e4305810537a1adb933aba6f9f40
Li.Meng <1784238945@qq.com>
2021-12-01 17:36:03 +0800

    push

--- scr/org/devops/tools.groovy
+++ scr/org/devops/tools.groovy
@@ -1,11 +1,13 @@
 package org.devops
 
-def printcolor(value,color){
-    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
-              'blue'  : "\033[47;34m ${value} \033[0m",
-              'green' : "^[[1;32m>>>>>>>>>>${value}>>>>>>>>>>^[[m",
-              'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
-    ansiColor('xterm') {
-        return colors[color]
+class Tools implements Serializable {
+    def printcolor(value,color){
+        colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+                'blue'  : "\033[47;34m ${value} \033[0m",
+                'green' : "^[[1;32m>>>>>>>>>>${value}>>>>>>>>>>^[[m",
+                'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+        ansiColor('xterm') {
+            return colors[color]
+        }
     }
 }
